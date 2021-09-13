7764cf2b04c29ccd74ec16ed39f939ba03bde0d3
Parker Byers <parker.n.byers@gmail.com>
2021-09-13 18:06:00 -0500

    mostly filled out ammo nav page. trouble with 7.62x54 rounds.

--- src/NavPages/Ammunition.js
+++ src/NavPages/Ammunition.js
@@ -38,7 +38,7 @@ function Ammunition () {
             <h1>Ammunition</h1>
             <div className='caliber-container'>
                 <h2 className='caliber'>5.56x45mm</h2>
-                <h5 className='compatible-weapons'>GUNS</h5>
+                <h5 className='compatible-weapons'>(M4A1 / HK / MDR / ADAR)</h5>
                 {ammo.map( (ammoData) => {
                     if (ammoData.name.split(' ')[0] === '5.56x45mm') {
                         return(
@@ -61,6 +61,7 @@ function Ammunition () {
             </div>
             <div className='caliber-container'>
                 <h2 className='caliber'>7.62x39mm</h2>
+                <h5 className='compatible-weapons'>(Mk47 / AK-103 / AKM)</h5>
                 {ammo.map( (ammoData) => {
                     if (ammoData.name.split(' ')[0] === '7.62x39mm') {
                         return(
@@ -83,8 +84,9 @@ function Ammunition () {
             </div>
             <div className='caliber-container'>
                 <h2 className='caliber'>7.62x51mm</h2>
+                <h5 className='compatible-weapons'>(M1A / SR-25 / SA-58 / MDR / M700 / Vepr Hunter)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '7.62x51mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -104,9 +106,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>7.62x54mm</h2>
+                <h5 className='compatible-weapons'>GUNS</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '7.62x54mmR') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -126,9 +129,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>4.6x30mm</h2>
+                <h5 className='compatible-weapons'>(MP7)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '4.6x30mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -148,9 +152,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>5.45x39mm</h2>
+                <h5 className='compatible-weapons'>(RPK / AK-105 / AK-74 / AKS-74)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '5.45x39mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -170,9 +175,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>5.7x28mm</h2>
+                <h5 className='compatible-weapons'>(P90 / 5-7)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '5.7x28mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -192,9 +198,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>9x19mm</h2>
+                <h5 className='compatible-weapons'>(MP5 / Vector / PP-19 / Glock)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '9x19mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -214,9 +221,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>9x39mm</h2>
+                <h5 className='compatible-weapons'>(AS VAL / VSS)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if ((ammoData.name.split(' ')[0] === '9x39mm') || (ammoData.name.split(' ')[0] === '9x39')) {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -236,9 +244,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>.338</h2>
+                <h5 className='compatible-weapons'>(MK-18)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '.338') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -258,9 +267,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>.366 TKM</h2>
+                <h5 className='compatible-weapons'>(VPO)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '.366') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -280,9 +290,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>.300</h2>
+                <h5 className='compatible-weapons'>(MCX)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '.300') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -302,9 +313,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>12.7x55mm</h2>
+                <h5 className='compatible-weapons'>(ASh-12)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '12.7x55') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -324,9 +336,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>.45 ACP</h2>
+                <h5 className='compatible-weapons'>(UMP-45 / Vector)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '.45') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -346,9 +359,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>12 Gauge</h2>
+                <h5 className='compatible-weapons'>(MP-153 / Saiga)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '12/70') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -368,9 +382,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>23x75mm</h2>
+                <h5 className='compatible-weapons'>(KS-23M)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '23x75mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -390,9 +405,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>20 Gauge</h2>
+                <h5 className='compatible-weapons'>GUNS</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '20/70') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -412,9 +428,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>9x21mm</h2>
+                <h5 className='compatible-weapons'>(SR-1MP)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '9x21mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -434,9 +451,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>9x18mm</h2>
+                <h5 className='compatible-weapons'>(Kedr / Makarov)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '9x18mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
@@ -456,9 +474,10 @@ function Ammunition () {
                 })}
             </div>
             <div className='caliber-container'>
-                <h2 className='caliber'>INSERTCALIBER</h2>
+                <h2 className='caliber'>7.62x25mm</h2>
+                <h5 className='compatible-weapons'>(TT)</h5>
                 {ammo.map( (ammoData) => {
-                    if (ammoData.name.split(' ')[0] === 'INSERTCALIBER') {
+                    if (ammoData.name.split(' ')[0] === '7.62x25mm') {
                         return(
                             <div className='round-container' key={ammoData.id}>
                                 <Link
